7d254c3c25d8a528869d8fa47bf824849612d11b
kirill.kiselev <net.k.kiselev@bergmann-infotech.de>
2022-01-10 12:23:17 +0300

    changes

--- BergmannStudy/CubeFamilyCommand.cs
+++ BergmannStudy/CubeFamilyCommand.cs
@@ -2,34 +2,41 @@
 using Autodesk.Revit.Attributes;
 using System.Collections.Generic;
 using System.Linq;
+using ArCm.ElementCreation.Implementation;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
 
-namespace ArCm
-{
-    [TransactionAttribute(TransactionMode.Manual)]
-    [Regeneration(RegenerationOption.Manual)]
-    public class CubeFamilyCommand : IExternalCommand
-    {
-        static AddInId addId = new AddInId(new Guid("DB33B495-E5B8-4557-B943-F52213965DB0"));
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
-            var uiApp = commandData.Application;
-            var app = commandData.Application.Application;
-            var uidoc = uiApp.ActiveUIDocument;
-            var doc = uidoc.Document;
+namespace ArCm{
+	[TransactionAttribute(TransactionMode.Manual)]
+	[Regeneration(RegenerationOption.Manual)]
+	public class CubeFamilyCommand : IExternalCommand{
+		static AddInId addId = new AddInId(new Guid("DB33B495-E5B8-4557-B943-F52213965DB0"));
 
-            Functions.FamilyOperations.CreateNewFamily(app, uiApp, "MyCube", @"C:\Users\79518\Desktop\Families\Generic Model.rft");
-            uidoc = uiApp.ActiveUIDocument;
-            doc = uidoc.Document;
-            Transaction t = new Transaction(doc, "extrusion");
-            using (t)
-            {
-                t.Start();
-                BergmannStudy.Ribbon.MyCube.MyCubeExecute(doc);
-                t.Commit();
-            }
-            return Result.Succeeded;
-        }
-    }
+		public Result Execute(ExternalCommandData commandData
+		                      , ref string        message
+		                      , ElementSet        elements) {
+			var uiApp = commandData.Application;
+			var app   = commandData.Application.Application;
+			var uidoc = uiApp.ActiveUIDocument;
+			var doc   = uidoc.Document;
+
+			Functions.FamilyOperations.CreateNewFamily(app
+			                                           , uiApp
+			                                           , "MyCube"
+			                                           , @"C:\Users\79518\Desktop\Families\Generic Model.rft");
+			uidoc = uiApp.ActiveUIDocument;
+			doc   = uidoc.Document;
+			Transaction t = new Transaction(doc, "extrusion");
+			using (t) {
+				t.Start();
+
+				BergmannStudy.Ribbon.MyCube.MyCubeExecute(doc);
+
+
+				t.Commit();
+			}
+
+			return Result.Succeeded;
+		}
+	}
 }
---
+++ BergmannStudy/ElementCreation/Interfaces/AbstractGenericFormCreator.cs
@@ -0,0 +1,16 @@
+﻿using ArCm.ElementCreation.Implementation;
+using Autodesk.Revit.DB;
+
+namespace ArCm.ElementCreation.Interfaces{
+	public abstract class AbstractGenericFormCreator<T> : IGenericFormCreator<T>{
+		protected readonly Document _Doc;
+
+		protected AbstractGenericFormCreator(Document doc, T properties) {
+			_Doc  = doc;
+			Props = properties;
+		}
+
+		public abstract GenericForm CreateForm();
+		public          T           Props { get; set; }
+	}
+}
