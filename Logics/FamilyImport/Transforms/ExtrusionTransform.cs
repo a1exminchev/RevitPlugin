a03be7c39fa2bdb9bfdfdfe67902f5a51e101a08
Aleksey Minchev <a1exmin@rambler.ru>
2022-01-27 23:53:20 +0300

    28.01.22

--- Logics/FamilyImport/Transforms/ExtrusionTransform.cs
+++ Logics/FamilyImport/Transforms/ExtrusionTransform.cs
@@ -28,41 +28,60 @@ namespace Logics.FamilyImport.Transforms
 			exParams.SketchPlane = rev.SketchPlane.Create(docToImport, rev.Plane.CreateByNormalAndOrigin(skOrigin, skNormal));
 
 			rev.CurveArrArray curArrArr = new rev.CurveArrArray();
+			rev.CurveArray c1 = new rev.CurveArray();
+			rev.CurveArray c2 = new rev.CurveArray();
+			rev.CurveArray c3 = new rev.CurveArray();
+			rev.CurveArray c4 = new rev.CurveArray();
+			rev.CurveArray c5 = new rev.CurveArray();
+			rev.CurveArray[] arrs = { c1, c2, c3, c4, c5};
+
 			int numArr = 1;
 			int numLine = 1;
+			int arrays = 0;
+			
 			foreach (var pair in CurveArrArray)
             {
 				if (pair.Key.Split('y')[1].Split('P')[0] == numArr.ToString())
                 {
-					rev.CurveArray curArr = new rev.CurveArray();
+					arrays += 1;
+					numArr += 1;
+                }
+				else
+                {
+					numArr = 1;
+                }
+            }
+			for(int i = 0; i <= arrays; i++)
+            {
+				arrs.Append(new rev.CurveArray());
+            }
+			foreach (var pair in CurveArrArray)
+            {
+				if (pair.Key.Split('y')[1].Split('P')[0] == numArr.ToString())
+                {
 					if (pair.Key.Split('e')[1] == numLine.ToString())
                     {
-						curArr.Append(rev.Line.CreateBound(new rev.XYZ(CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][0],
+						arrs[numArr - 1].Append(rev.Line.CreateBound(new rev.XYZ(CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][0],
 																   CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][1],
 																   CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][2]),
 													   new rev.XYZ(CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][3],
 																   CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][4],
 																   CurveArrArray[$"Array{numArr}PointOfLine{numLine}"][5])));
 						numLine += 1;
-						
-						goto P1;
 					}
 					else
                     {
-						curArrArr.Append(curArr);
+						curArrArr.Append(arrs[numArr - 1]);
+						numArr += 1;
 					}
-					numArr += 1;
-					
 				}
 				else
                 {
 					break;
                 }
-			P1:
-				;
 			}
 			exParams.curveArrArray = curArrArr;
-			//TaskDialog.Show("d", curArrArr.get_Item(0).get_Item(0).GetEndPoint(0).ToString());
+			TaskDialog.Show("d", curArrArr.get_Item(0).get_Item(1).GetEndPoint(0).ToString());
 			ExtrusionCreator extrusionCreator = new ExtrusionCreator(docToImport, exParams);
 			extrusionCreator.Create();
 			//Creation of extrusion in doc by props
